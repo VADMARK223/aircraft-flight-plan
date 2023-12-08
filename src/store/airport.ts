d14ff96756db429285fe604e497e5a22df9201ef
v.markitanov <v.markitanov@outlook.com>
2023-12-08 10:28:25 +0300

    Refactoring.

--- src/store/airport.ts
+++ src/store/airport.ts
@@ -4,57 +4,6 @@
  */
 import { createStore } from 'effector'
 import { Airport } from '../models/Airport'
+import { airportsDefault } from '../utils/consts'
 
-const defaultStore: Airport[] = [
-	{
-		name: 'Анапа, Краснодарский край',
-		country: 'Россия',
-		iata: 'AAQ',
-		icao: 'URKA'
-	},
-	{
-		name: 'Абакан, Республика Хакасия',
-		country: 'Россия',
-		iata: 'ABA',
-		icao: 'UNAA'
-	},
-	{
-		name: 'Ачинск, Красноярский край',
-		country: 'Россия',
-		iata: 'ACS',
-		icao: 'UNKS'
-	},
-	{
-		name: 'Алдан, Якутия',
-		country: 'Россия',
-		iata: 'ADH',
-		icao: 'UEEA'
-	},
-	{
-		name: 'Сочи, Краснодарский край',
-		country: 'Россия',
-		iata: 'AER',
-		icao: 'URSS'
-	},
-	{
-		name: 'Амдерма, Ненецкий АО',
-		country: 'Россия',
-		iata: 'AMV',
-		icao: 'ULDD'
-	},
-	{
-		name: 'Талаги, Архангельск',
-		country: 'Россия',
-		iata: 'ARH',
-		icao: 'ULAA'
-	},
-	{
-		name: 'Нариманово, Астрахань',
-		country: 'Россия',
-		iata: 'ASF',
-		icao: 'URWA'
-	}
-
-]
-
-export const $airports = createStore<Airport[]>(defaultStore)
+export const $airports = createStore<Airport[]>(airportsDefault)
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,5 +1,8 @@
-import { Flight } from '../models/Flight'
 import { Board } from '../models/Board'
+import { BoardType } from '../models/BoardType'
+import { FlightType } from '../models/FlightType'
+import dayjs from 'dayjs'
+import { Airport } from '../models/Airport'
 
 /**
  * @author Markitanov Vadim
@@ -20,16 +23,254 @@ export const SHOW_FLIGHT_ID = false
 export const SHOW_OLD_STICKS = false
 export const RESIZE_STICK_WIDTH = 5
 
-export const findFlightById = (id: string | undefined, boards: Board[]): Flight | undefined => {
-	let currentFlight: Flight | undefined
-	for (let i = 0; i < boards.length; i++) {
-		currentFlight = boards[i].flights.find(value => {
-			return value.id === id
-		})
-		if (currentFlight !== undefined) {
-			break
-		}
+export const boardsDefault: Board[] = [
+	{
+		id: 1,
+		name: 'Борт 1',
+		type: BoardType.LOW,
+		flights: [
+			{
+				id: '11',
+				boardId: 1,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(0, 'hours'),
+				endDate: dayjs().startOf('day').add(2, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			},
+			{
+				id: '21',
+				boardId: 1,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(23, 'hours'),
+				endDate: dayjs().startOf('day').add(25, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			},
+			{
+				id: '31',
+				boardId: 1,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(28, 'hours'),
+				endDate: dayjs().startOf('day').add(29, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			}
+		]
+	},
+	{
+		id: 2,
+		name: 'Борт 2',
+		type: BoardType.DEFAULT,
+		flights: [
+			{
+				id: '12',
+				boardId: 2,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(1, 'hours'),
+				endDate: dayjs().startOf('day').add(3, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			},
+			{
+				id: '22',
+				boardId: 2,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(5, 'hours'),
+				endDate: dayjs().startOf('day').add(6, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			},
+			{
+				id: '32',
+				boardId: 2,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(18, 'hours'),
+				endDate: dayjs().startOf('day').add(30, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			},
+			{
+				id: '42',
+				boardId: 2,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(32, 'hours'),
+				endDate: dayjs().startOf('day').add(33, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			}
+		]
+	},
+	{
+		id: 3,
+		name: 'Борт 3',
+		type: BoardType.LOW,
+		flights: [
+			{
+				id: '13',
+				boardId: 3,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(3, 'hours'),
+				endDate: dayjs().startOf('day').add(6, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			}
+		]
+	},
+	{
+		id: 4,
+		name: 'Борт 4',
+		type: BoardType.PRIORITY,
+		flights: [
+			{
+				id: '14',
+				boardId: 4,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(5, 'hours'),
+				endDate: dayjs().startOf('day').add(16, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			}
+		]
+	},
+	{
+		id: 5,
+		name: 'Борт 5',
+		type: BoardType.LOW,
+		flights: [
+			{
+				id: '15',
+				boardId: 5,
+				type: FlightType.ROUTINE_MAINTENANCE,
+				startDate: dayjs().startOf('day').add(15, 'hours'),
+				endDate: dayjs().startOf('day').add(30, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			}
+		]
+	},
+	{
+		id: 6,
+		name: 'Борт 6',
+		type: BoardType.DEFAULT,
+		flights: [
+			{
+				id: '16',
+				boardId: 6,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(11, 'hours'),
+				endDate: dayjs().startOf('day').add(14, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			},
+			{
+				id: '26',
+				boardId: 6,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(32, 'hours'),
+				endDate: dayjs().startOf('day').add(35, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			}
+		]
+	},
+	{
+		id: 7,
+		name: 'Борт 7',
+		type: BoardType.DEFAULT,
+		flights: [
+			{
+				id: '17',
+				boardId: 7,
+				type: FlightType.ROUTINE_MAINTENANCE,
+				startDate: dayjs().startOf('day').add(0, 'hours'),
+				endDate: dayjs().startOf('day').add(42, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			}
+		]
+	},
+	{
+		id: 8,
+		name: 'Борт 8',
+		type: BoardType.DEFAULT,
+		flights: [
+			{
+				id: '18',
+				boardId: 8,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(0.5, 'hours'),
+				endDate: dayjs().startOf('day').add(3.5, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			}
+		]
+	},
+	{
+		id: 9,
+		name: 'Борт 9',
+		type: BoardType.DEFAULT,
+		flights: [
+			{
+				id: '19',
+				boardId: 9,
+				type: FlightType.DEFAULT,
+				startDate: dayjs().startOf('day').add(1, 'hours'),
+				endDate: dayjs().startOf('day').add(3, 'hours'),
+				airportStart: 'AAQ',
+				airportEnd: 'ABA'
+			}
+		]
 	}
+]
 
-	return currentFlight
-}
+export const airportsDefault: Airport[] = [
+	{
+		name: 'Анапа, Краснодарский край',
+		country: 'Россия',
+		iata: 'AAQ',
+		icao: 'URKA'
+	},
+	{
+		name: 'Абакан, Республика Хакасия',
+		country: 'Россия',
+		iata: 'ABA',
+		icao: 'UNAA'
+	},
+	{
+		name: 'Ачинск, Красноярский край',
+		country: 'Россия',
+		iata: 'ACS',
+		icao: 'UNKS'
+	},
+	{
+		name: 'Алдан, Якутия',
+		country: 'Россия',
+		iata: 'ADH',
+		icao: 'UEEA'
+	},
+	{
+		name: 'Сочи, Краснодарский край',
+		country: 'Россия',
+		iata: 'AER',
+		icao: 'URSS'
+	},
+	{
+		name: 'Амдерма, Ненецкий АО',
+		country: 'Россия',
+		iata: 'AMV',
+		icao: 'ULDD'
+	},
+	{
+		name: 'Талаги, Архангельск',
+		country: 'Россия',
+		iata: 'ARH',
+		icao: 'ULAA'
+	},
+	{
+		name: 'Нариманово, Астрахань',
+		country: 'Россия',
+		iata: 'ASF',
+		icao: 'URWA'
+	}
+]

1c5483c2f131e704d57ed737e5d02582a4141a26
v.markitanov <v.markitanov@outlook.com>
2023-12-09 08:11:21 +0300

    Hook for dragging svg.

--- src/components/geo/Geo.tsx
+++ src/components/geo/Geo.tsx
@@ -4,112 +4,48 @@
  * @author Markitanov Vadim
  * @since 08.12.2023
  */
-import { JSX, LegacyRef, useEffect, useRef, useState } from 'react'
-import * as d3 from 'd3'
-import { D3ZoomEvent } from 'd3'
+import { JSX, LegacyRef, useRef } from 'react'
+import { useDraggableSvg } from '../../hooks/useDraggableSvg'
 
-interface Dimensions {
-	width: number
-	height: number
-}
+const CANVAS_WIDTH = 300
+const CANVAS_HEIGHT = 300
+const WORKSPACE_SIZE = 500
+const WORKSPACE_WIDTH = WORKSPACE_SIZE
+const WORKSPACE_HEIGHT = WORKSPACE_SIZE
 
 const Geo = (): JSX.Element => {
 	const svgRef: LegacyRef<SVGSVGElement> = useRef<SVGSVGElement>(null)
-	const [dimensions, setDimensions] = useState<Dimensions>({ width: 0, height: 0 })
-	const CANVAS_WIDTH = 300
-	const CANVAS_HEIGHT = 300
-
-	const WORKSPACE_SIZE = 350
-	const WORKSPACE_WIDTH = WORKSPACE_SIZE
-	const WORKSPACE_HEIGHT = WORKSPACE_SIZE
-
-	useEffect(() => {
-		const observableTarget = svgRef.current
-		if (observableTarget === null) {
-			return
-		}
-
-		const svg = d3.select(svgRef.current)
-		svg.attr('viewBox', [0, 0, CANVAS_WIDTH, CANVAS_HEIGHT])
-		svg.attr('width', CANVAS_WIDTH)
-		svg.attr('height', CANVAS_HEIGHT)
-
-		const g = svg.select('g')
-		g.attr('cursor', 'pointer')
-
-		g.append('rect')
-			.attr('id', 'TARGET')
-			.attr('width', WORKSPACE_WIDTH)
-			.attr('height', WORKSPACE_HEIGHT)
-			.attr('fill', 'green')
-
-		g.append('line')
-			.attr('x1', 0)
-			.attr('y1', 0)
-			.attr('x2', WORKSPACE_WIDTH)
-			.attr('y2', WORKSPACE_HEIGHT)
-			.attr('stroke', 'white')
-
-		g.append('line')
-			.attr('x1', 0)
-			.attr('y1', WORKSPACE_HEIGHT)
-			.attr('x2', WORKSPACE_WIDTH)
-			.attr('y2', 0)
-			.attr('stroke', 'white')
-
-		const resizeObserver = new ResizeObserver(entries => {
-			entries.forEach(value => {
-				setDimensions(value.contentRect)
-			})
-		})
-		resizeObserver.observe(observableTarget)
-		return () => resizeObserver.unobserve(observableTarget)
-	}, [svgRef, WORKSPACE_WIDTH, WORKSPACE_HEIGHT])
-
-	useEffect(() => {
-		const svg: any = d3.select(svgRef.current)
-		const g = svg.select('g')
-
-		const zoomed = (event: D3ZoomEvent<any, any>) => {
-			const transform = event.transform
-			let x = transform.x
-			let y = transform.y
-			const k = 1
-			if (x >= 0) {
-				x = 0
-			}
-			if (y >= 0) {
-				y = 0
-			}
-
-			const tempX = WORKSPACE_WIDTH - CANVAS_WIDTH + x
-			if (tempX <= 0) {
-				x = CANVAS_WIDTH - WORKSPACE_WIDTH
-			}
-
-			const tempY = WORKSPACE_HEIGHT - CANVAS_HEIGHT + y
-			if (tempY <= 0) {
-				y = CANVAS_HEIGHT - WORKSPACE_HEIGHT
-			}
-
-			g.attr('transform', `translate(${x}, ${y}) scale(${k})`)
-		}
-
-		if (WORKSPACE_WIDTH > CANVAS_WIDTH || WORKSPACE_HEIGHT > CANVAS_HEIGHT) {
-			svg.call(d3.zoom()
-				// .extent([[0, 0], [dimensions.width, dimensions.height]])
-				.scaleExtent([0.5, 2]) // Ограничения zoom (Можно уменьшить в  половину и у величтиь в 2 раза)
-				.on('zoom', zoomed))
-		}
-		else {
-			console.log('NOT')
-		}
-	}, [dimensions, WORKSPACE_WIDTH, WORKSPACE_HEIGHT])
+	useDraggableSvg(svgRef)
 
 	return (
-		<svg ref={svgRef}>
-			<g></g>
-			Component Geo
+		<svg ref={svgRef} style={{
+			width: CANVAS_WIDTH,
+			height: CANVAS_HEIGHT
+			// width: '100vw',
+			// height: '100vh'
+		}}>
+			<g cursor={'pointer'} id={'TARGET'}>
+				<rect
+					// style={{ width: '200vw', height: '200vh' }}
+					width={WORKSPACE_WIDTH}
+					height={WORKSPACE_HEIGHT}
+					fill={'green'}
+				/>
+				<line
+					x2={WORKSPACE_WIDTH}
+					y2={WORKSPACE_HEIGHT}
+					// x2={'200vw'}
+					// y2={'200vh'}
+					stroke={'white'}
+				/>
+				<line
+					y1={WORKSPACE_HEIGHT}
+					x2={WORKSPACE_WIDTH}
+					// y1={'200vh'}
+					// x2={'200vw'}
+					stroke={'white'}
+				/>
+			</g>
 		</svg>
 	)
 }
---
+++ src/hooks/useDraggableSvg.ts
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react'
+import * as d3 from 'd3'
+import { D3ZoomEvent, ZoomTransform } from 'd3'
+
+/**
+ * @author Markitanov Vadim
+ * @since 09.12.2023
+ */
+
+export const useDraggableSvg = (ref: React.RefObject<SVGSVGElement>) => {
+	const [dimensions, setDimensions] = useState<DOMRectReadOnly>({
+		x: 0,
+		y: 0,
+		width: 0,
+		height: 0,
+		top: 0,
+		right: 0,
+		bottom: 0,
+		left: 0,
+		toJSON (): any {
+		}
+	})
+
+	useEffect(() => {
+		const observeTarget = ref.current
+		if (observeTarget === null) {
+			return
+		}
+		const resizeObserver = new ResizeObserver(entries => {
+			entries.forEach(entry => {
+				setDimensions(entry.contentRect)
+			})
+		})
+		resizeObserver.observe(observeTarget)
+		return () => resizeObserver.unobserve(observeTarget)
+	}, [ref])
+
+	useEffect(() => {
+		const svg: any = d3.select(ref.current)
+		const g: any = svg.select('g')
+		const box = g.node().getBBox()
+
+		const groupWidth = box.width
+		const groupHeight = box.height
+
+		svg.attr('width', dimensions.width).attr('height', dimensions.height)
+
+		const onZoom = (event: D3ZoomEvent<SVGSVGElement, undefined>) => {
+			const transform: ZoomTransform = event.transform
+			let x = transform.x >= 0 ? 0 : transform.x
+			let y = transform.y >= 0 ? 0 : transform.y
+			const tempX = groupWidth - dimensions.width + x
+			if (tempX <= 0) {
+				x = dimensions.width - groupWidth
+			}
+
+			const tempY = groupHeight - dimensions.height + y
+			if (tempY <= 0) {
+				y = dimensions.height - groupHeight
+			}
+
+			g.attr('transform', `translate(${x}, ${y})`)
+		}
+
+		if (groupWidth > dimensions.width || groupHeight > dimensions.height) {
+			svg.call(d3.zoom()
+				.extent([[0, 0], [dimensions.width, dimensions.height]])
+				.scaleExtent([0.5, 2])
+				.on('zoom', onZoom))
+		}
+	}, [ref, dimensions])
+
+	return dimensions
+}

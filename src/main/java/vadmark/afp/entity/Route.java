c365d85f1b5fe26dae92aae5f9e60b4bf7551b2d
v.markitanov <v.markitanov@outlook.com>
2024-11-09 10:13:17 +0300

    Inserts.

---
+++ src/main/java/vadmark/afp/entity/Route.java
@@ -0,0 +1,22 @@
+package vadmark.afp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Markitanov Vadim
+ * @since 09.11.2024
+ */
+@Entity
+@Table(schema = "afp_schema", name = "route")
+@Getter
+@Setter
+public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "route_id")
+    private int routeId;
+
+
+}
---
+++ src/main/resources/db/migration/V2__Inserts_data.sql
@@ -0,0 +1,35 @@
+INSERT INTO afp_schema.dict_route_type VALUES (1, 'Обычный');
+INSERT INTO afp_schema.dict_route_type VALUES (2, 'Грузовой');
+
+INSERT INTO afp_schema.flight (contact_id) VALUES (999);
+INSERT INTO afp_schema.aircraft (aircraft_name, aircraft_type_id, tail) VALUES ('Boeng', 3,'tail');
+INSERT INTO afp_schema.aircraft (aircraft_name, aircraft_type_id, tail) VALUES ('Airbus', 4,'tail4');
+
+INSERT INTO afp_schema.airport VALUES (1, 'VKO', 'UUWW ', 'Внуково');
+INSERT INTO afp_schema.airport VALUES (2, 'SVO', 'UUEE ', 'Шереметьево');
+
+DO
+$$
+    DECLARE
+        i             INTEGER;
+        random_number INTEGER;
+    BEGIN
+        FOR i IN 1..10
+            LOOP
+                random_number := TRUNC(RANDOM() * 1000) + 1;
+                INSERT INTO afp_schema.route (airport_departure, airport_arrival, weight_load, weight_offload,
+                                              scheduled_departure_date, scheduled_arrival_date, actual_departure_date,
+                                              actual_arrival_date, route_type_id, flight_number, aircraft_id, flight_id,
+                                              fuel_uplift)
+                VALUES (CASE
+                            WHEN RANDOM() > 0.5 THEN 1
+                            ELSE 2
+                            END,
+                        CASE
+                            WHEN RANDOM() > 0.5 THEN 1
+                            ELSE 2
+                            END,
+                        random_number, 5, NOW(), NOW(), NOW(), NOW(), 1, 'FN', 1, 1, 5);
+            END LOOP;
+    END
+$$;

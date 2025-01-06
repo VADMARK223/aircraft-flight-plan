daf8e4a6e5a8ee4effaa7e63c4362db55cae2838
v.markitanov <vadmark223@mail.ru>
2025-01-06 20:39:01 +0300

    Migrate react-router-dom.

--- client/afp/src/App.tsx
+++ client/afp/src/App.tsx
@@ -47,17 +47,7 @@ function App () {
 					{ path: 'stats', element: <StatsPage/> }
 				],
 			}
-		],
-		{
-			future: {
-				// v7_startTransition: true,
-				v7_relativeSplatPath: true,
-				v7_fetcherPersist: true,
-				v7_normalizeFormMethod: true,
-				v7_partialHydration: true,
-				v7_skipActionErrorRevalidation: true
-			}
-		}
+		]
 	)
 
 	return (

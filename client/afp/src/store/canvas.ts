b2ed859fc709a34f98159178469aa2d134d70f1f
v.markitanov <vadmark223@mail.ru>
2025-02-09 10:07:38 +0300

    hoursInCell в хранилище канвы.

--- client/afp/src/store/canvas.ts
+++ client/afp/src/store/canvas.ts
@@ -1,13 +1,13 @@
 /**
+ * Хранилище данных канвы.
+ *
  * @author Markitanov Vadim
  * @since 08.02.2025
  */
 import { createStore, createEvent } from 'effector'
 import dayjs, { Dayjs } from 'dayjs'
 import type { RangeValueType } from 'rc-picker/lib/PickerInput/RangePicker'
-import { createEffect } from 'effector/compat'
 import { DateModel } from '../models/DateModel'
-import { HOURS_IN_CELL } from '../utils/consts'
 import { getWeekCount } from '../utils/utils'
 
 export enum ZoomMode {
@@ -19,41 +19,42 @@ export enum ZoomMode {
 interface CanvasStore {
 	zoomMode: ZoomMode
 	dateRange: RangeValueType<Dayjs>
-	dates: DateModel[]
+	dates: DateModel[],
+	hoursInCell: number // Сколько часов в ячейке на канве
 }
 
 const defaultState: CanvasStore = {
 	zoomMode: ZoomMode.DAY,
 	dateRange: [dayjs().startOf('day'), dayjs().add(1, 'days').startOf('day')],
-	dates: []
+	dates: [],
+	/**
+	 * 6 - для Day
+	 * 12 - для Week
+	 * 24 - для Days
+	 * @type {number}
+	 */
+	hoursInCell: 6
 }
 
 export const zoomModeChanged = createEvent<ZoomMode>('Событие изменения масштабирования канвы.')
 export const datesRangeChanged = createEvent<RangeValueType<Dayjs>>('Событие изменения периода.')
-const updateDatesFx = createEffect<DateModel[], DateModel[]>()
 
 export const $canvas = createStore<CanvasStore>(defaultState)
 $canvas.on(zoomModeChanged, (state, payload) => {
 	return { ...state, zoomMode: payload }
 })
-$canvas.on(datesRangeChanged, (state, payload) => {
-	return { ...state, dateRange: payload }
-})
-$canvas.on(updateDatesFx, (state, payload) => {
-	return { ...state, dates: payload }
-})
-
-datesRangeChanged.watch((datesRange) => {
+$canvas.on(datesRangeChanged, (state, datesRange) => {
+	const newDates: DateModel[] = []
 	if (datesRange && datesRange[0] && datesRange[1]) {
 		const newStartDate: Dayjs = datesRange[0]
-		const diffHours = datesRange[1].add(1, 'day').diff(newStartDate, 'hours') / HOURS_IN_CELL
-		const newDates: DateModel[] = []
+		const diffHours = datesRange[1].add(1, 'day').diff(newStartDate, 'hours') / state.hoursInCell
 		for (let i = 0; i < diffHours; i++) {
-			const newDate = newStartDate.add(i * HOURS_IN_CELL, 'hours')
+			const newDate = newStartDate.add(i * state.hoursInCell, 'hours')
 			newDates.push({
 				date: newDate, title: `(W${getWeekCount(newDate)})`
 			})
 		}
-		updateDatesFx(newDates)
 	}
+
+	return { ...state, dateRange: datesRange, dates: newDates }
 })
